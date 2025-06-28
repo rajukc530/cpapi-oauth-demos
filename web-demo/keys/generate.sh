92f7132edc7dca2385464f51dc1d88e30bb1bd86
Raju <rajukc530@gmail.com>
2025-06-29 01:44:34 +0545

    -my testing keys..

--- web-demo/keys/generate.sh
+++ web-demo/keys/generate.sh
@@ -1,4 +1,4 @@
-openssl dhparam -outform PEM 2048 -out dhparam.pem
+openssl dhparam -outform PEM -out dhparam.pem 2048
 openssl genrsa -out private_signature.pem 2048
 openssl rsa -in private_signature.pem -outform PEM -pubout -out public_signature.pem
 openssl genrsa -out private_encryption.pem 2048
